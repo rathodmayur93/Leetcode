56c11d77f7b68572c7d3eb8acfa2b41e7676d4ee
Mayur Rathod <rathodmayur93@gmail.com>
2020-12-17 17:48:55 +0300

    Solved: CanPlaceFlowers
    - 100 Problem Of Array (Easy) solved

--- Leetcode/Problems/Array/Easy/CanPlaceFlowers.swift
+++ Leetcode/Problems/Array/Easy/CanPlaceFlowers.swift
@@ -11,5 +11,10 @@ struct CanPlaceFlowers {
     
     func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) {
         
+        let flowerDic = [Int : Int]()
+        
+        flowerDic = flowerbed.reduce(into: dic, { ([:], value) in
+            dic[value, default: 0] += 1
+        })
     }
 }
